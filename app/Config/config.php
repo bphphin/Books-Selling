e485dcc0565bd8e75d932578cef196d254affabe
bphphin <buingocphinn@gmail.com>
2023-07-24 19:48:57 +0700

    base

---
+++ app/Config/config.php
@@ -0,0 +1,7 @@
+<?php
+const DSN = "mysql:host=localhost;dbname=du_an_1;charset=utf8";
+const USER = "root";
+const PASSWORD = "";
+const URL = "http://localhost/fpt/duan1-fpt/";
+define('APP_ROOT',dirname(__DIR__));
+?>
